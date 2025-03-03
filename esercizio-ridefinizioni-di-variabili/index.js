a12c67a0875ff399f18ff3595e956d459c8aebb0
manu895 <manuel_bologna@yahoo.it>
2025-03-03 10:58:40 +0100

    variable

---
+++ esercizio-ridefinizioni-di-variabili/index.js
@@ -0,0 +1,12 @@
+let number = 10;
+
+
+{ 
+   
+    let number = 13;
+
+    console.log("Valore di number all'interno del blocco:", number);
+}
+
+
+console.log("Valore di number all'esterno:", number);
