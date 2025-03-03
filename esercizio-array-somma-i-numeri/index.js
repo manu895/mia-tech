641451b273da9ca4854f3d06a6eb950d41e5bbae
manu895 <manuel_bologna@yahoo.it>
2025-03-03 18:51:54 +0100

    sum-array

---
+++ esercizio-array-somma-i-numeri/index.js
@@ -0,0 +1,9 @@
+let numeri = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+let somma = 0;
+
+for (let i = 0; i < numeri.length; i++) {
+    somma += numeri[i];
+}
+
+console.log("La somma dei numeri è: " + somma);
