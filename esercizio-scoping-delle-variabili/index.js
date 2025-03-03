36fab0866756ae191ac55947b8cb2ba4445d873b
manu895 <manuel_bologna@yahoo.it>
2025-03-03 11:06:56 +0100

    variable-scoping

---
+++ esercizio-scoping-delle-variabili/index.js
@@ -0,0 +1,17 @@
+
+let outside = 5;
+
+
+{
+    
+    let inside = 23;
+    
+    console.log(outside);
+    console.log(inside);
+}
+
+
+console.log(outside);
+
+// inside non è dichiarato fuori dal blocco dando errore
+console.log(inside); 
