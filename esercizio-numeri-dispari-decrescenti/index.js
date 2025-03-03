0bac526ec4d46144981151ba219ef32463f57fde
manu895 <manuel_bologna@yahoo.it>
2025-03-03 11:57:37 +0100

    use-whyle

---
+++ esercizio-numeri-dispari-decrescenti/index.js
@@ -0,0 +1,6 @@
+let num = 9; // partiamo da 9 perche è il primo numero dispari sotto il 10
+
+while (num >= 1) {
+    console.log(num);
+    num -= 2; // Decrementiamo di 2 per ottenere solo numeri dispari
+}
