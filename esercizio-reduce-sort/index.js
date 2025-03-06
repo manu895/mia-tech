1c5dfd2424104a6fb6e124b4db8c2ba5c63ca28d
manu895 <manuel_bologna@yahoo.it>
2025-03-06 11:03:14 +0100

    reduce-sort

---
+++ esercizio-reduce-sort/index.js
@@ -0,0 +1,12 @@
+let expenses = [50, 100, 25, 75, 30];
+
+let totalExpenses = expenses.reduce((sum, expense) => sum + expense, 0);
+
+let words = ["banana", "apple", "cherry", "date"];
+
+words.sort();
+
+console.log("Somma totale delle spese:", totalExpenses);
+
+console.log("Parole ordinate:", words);	
+
