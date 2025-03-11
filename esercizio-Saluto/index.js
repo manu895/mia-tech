d882f9fa0f6096d342126e9317a815465014e1e0
manu895 <manuel_bologna@yahoo.it>
2025-03-11 21:29:58 +0100

    update-hello

---
+++ esercizio-Saluto/index.js
@@ -0,0 +1,69 @@
+class Automobile {
+    constructor(marca, modello, anno) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = 0;
+    }
+
+    descrizione() {
+        return this.anno + " " + this.marca + " " + this.modello;
+    }
+
+    aggiungiChilometri(km) {
+        if (km > 0) {
+            this.chilometraggio += km;
+        }
+    }
+
+    mostraChilometraggio() {
+        return "Chilometri: " + this.chilometraggio + " km";
+    }
+}
+
+//metodo saluta() al prototype di Automobile
+Automobile.prototype.saluta = function() {
+    return `Ciao! Sono una ${this.marca} ${this.modello}.`;
+};
+
+
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, autonomia) {
+        super(marca, modello, anno);
+        this.autonomia = autonomia;
+    }
+
+    descrizione() {
+        return this.anno + " " + this.marca + " " + this.modello + " - Autonomia: " + this.autonomia + " km";
+    }
+
+    ricarica(km) {
+        if (km > 0) {
+            this.autonomia += km;
+        }
+    }
+}
+
+
+const miaAuto = new Automobile("Jeep", "Renegade", 2018);
+console.log(miaAuto.saluta());
+
+console.log(miaAuto.descrizione());
+
+console.log(miaAuto.mostraChilometraggio());
+
+miaAuto.aggiungiChilometri(90000);
+
+console.log(miaAuto.mostraChilometraggio());
+
+
+
+const miaAutoElettrica = new Elettrica("Tesla", "Model 3", 2023, 400);
+
+console.log(miaAutoElettrica.saluta());
+
+console.log(miaAutoElettrica.descrizione());
+
+miaAutoElettrica.ricarica(100);
+
+console.log(miaAutoElettrica.descrizione());
