07e1078302de077d2c57cabc5046df4eff171c17
manu895 <manuel_bologna@yahoo.it>
2025-03-12 20:35:06 +0100

    contatore

---
+++ esercizio-mostra-contatore/index.js
@@ -0,0 +1,149 @@
+class Automobile {
+    #contatoreChiamate = 0; // Contatore privato per tutti i metodi
+    #contatoreAggiungiChilometri = 0; // Contatore specifico per aggiungiChilometri()
+
+    constructor(marca, modello, anno) {
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = 0;
+    }
+
+    // Metodo privato per incrementare il contatore generale
+    #incrementaContatore() {
+        this.#contatoreChiamate += 1;
+    }
+
+    // Metodo per ottenere il numero di chiamate totali
+    mostraContatoreChiamate() {
+        return `Numero di chiamate ai metodi: ${this.#contatoreChiamate}`;
+    }
+
+    // Metodo per ottenere il numero di chiamate specifiche a aggiungiChilometri()
+    mostraContatoreAggiungiChilometri() {
+        return `Il metodo aggiungiChilometri è stato chiamato ${this.#contatoreAggiungiChilometri} volte.`;
+    }
+
+    descrizione() {
+        this.#incrementaContatore();
+        return this.anno + " " + this.marca + " " + this.modello;
+    }
+
+    aggiungiChilometri(km) {
+        this.#incrementaContatore();
+        this.#contatoreAggiungiChilometri += 1; // Incrementa il contatore specifico
+        if (km > 0) {
+            this.chilometraggio += km;
+        }
+    }
+
+    mostraChilometraggio() {
+        this.#incrementaContatore();
+        return "Chilometri: " + this.chilometraggio + " km";
+    }
+
+    calcolaEtà() {
+        const annoCorrente = new Date().getFullYear();
+        return annoCorrente - this.anno;
+    }
+
+    mostraEtà() {
+        return `L'età dell'automobile è di ${this.calcolaEtà()} anni.`;
+    }
+
+    controllaChilometraggio() {
+        if (this.chilometraggio > 100000) {
+            return "Attenzione! Il chilometraggio ha superato i 100,000 km.";
+        }
+        return "Il chilometraggio è sotto il limite.";
+    }
+
+    static confrontaChilometraggio(auto1, auto2) {
+        if (auto1.chilometraggio > auto2.chilometraggio) {
+            return `${auto1.marca} ${auto1.modello} ha un chilometraggio maggiore di ${auto2.marca} ${auto2.modello}.`;
+        } else if (auto1.chilometraggio < auto2.chilometraggio) {
+            return `${auto2.marca} ${auto2.modello} ha un chilometraggio maggiore di ${auto1.marca} ${auto1.modello}.`;
+        } else {
+            return `Le due automobili hanno lo stesso chilometraggio.`;
+        }
+    }
+
+    aggiornaChilometri(km) {
+        this.aggiungiChilometri(km);
+    }
+}
+
+// Metodo saluta() al prototype di Automobile
+Automobile.prototype.saluta = function() {
+    return `Ciao! Sono una ${this.marca} ${this.modello}.`;
+};
+
+class Elettrica extends Automobile {
+    constructor(marca, modello, anno, autonomia) {
+        super(marca, modello, anno);
+        this.autonomia = autonomia;
+    }
+
+    descrizione() {
+        return this.anno + " " + this.marca + " " + this.modello + " - Autonomia: " + this.autonomia + " km";
+    }
+
+    ricarica(km) {
+        if (km > 0) {
+            this.autonomia += km;
+        }
+    }
+
+    verificaChilometraggio() {
+        console.log(this.controllaChilometraggio());
+    }
+}
+
+// Test con l'auto Jeep Renegade
+const miaAuto = new Automobile("Jeep", "Renegade", 2018);
+
+console.log(miaAuto.saluta());
+
+console.log(miaAuto.descrizione());
+
+console.log(miaAuto.mostraChilometraggio());
+
+miaAuto.aggiungiChilometri(90000);
+
+miaAuto.aggiungiChilometri(10000);
+
+console.log(miaAuto.mostraChilometraggio());
+
+console.log(miaAuto.mostraEtà());
+
+console.log(miaAuto.controllaChilometraggio());
+
+
+// Test con l'auto Tesla Model 3
+const miaAutoElettrica = new Elettrica("Tesla", "Model 3", 2023, 400);
+
+console.log(miaAutoElettrica.saluta());
+
+console.log(miaAutoElettrica.descrizione());
+
+miaAutoElettrica.ricarica(100);
+
+console.log(miaAutoElettrica.descrizione());
+
+miaAutoElettrica.aggiungiChilometri(120000);
+
+miaAutoElettrica.verificaChilometraggio();
+
+
+// Confronto dei chilometri tra le due auto
+console.log(Automobile.confrontaChilometraggio(miaAuto, miaAutoElettrica));
+
+// Mostra il numero totale di chiamate a tutti i metodi
+console.log(miaAuto.mostraContatoreChiamate());
+
+// Mostra il numero di volte che è stato chiamato solo aggiungiChilometri()
+console.log(miaAuto.mostraContatoreAggiungiChilometri());
+
+console.log(miaAutoElettrica.mostraContatoreChiamate());
+
+console.log(miaAutoElettrica.mostraContatoreAggiungiChilometri());
