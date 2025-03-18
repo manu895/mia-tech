45448203fe5d6a1eeeae71aea182be8bd5f6c514
manu895 <manuel_bologna@yahoo.it>
2025-03-18 18:39:33 +0100

    oggetto-con-proprieta'

---
+++ esercizio copia di un oggetto con proprieta' aggiuntive/index.js
@@ -0,0 +1,16 @@
+let oggettoOriginale = {
+    nome: "Manuel",
+    età: 33
+  };
+
+  
+  let copiaOggetto = {
+    ...oggettoOriginale,
+    professione: "Sviluppatore"
+  };
+
+  
+  console.log("Oggetto Originale:", oggettoOriginale);
+
+  console.log("Copia Modificata:", copiaOggetto);
+  
