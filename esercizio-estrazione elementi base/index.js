8a1c01a4a8ad63651acd4932b9afed5fe4e0fbd1
manu895 <manuel_bologna@yahoo.it>
2025-03-18 18:28:38 +0100

    element

---
+++ esercizio-estrazione elementi base/index.js
@@ -0,0 +1,8 @@
+
+const numeri = [10, 20, 30, 40, 50];
+
+const [primo, secondo] = numeri;
+
+console.log("Primo elemento:", primo); // Output: Primo elemento: 10
+
+console.log("Secondo elemento:", secondo); // Output: Secondo elemento: 20
