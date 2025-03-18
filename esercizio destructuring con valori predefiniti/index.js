7ee341c4e90ca2f9da0f3351b5299a246acbf6f1
manu895 <manuel_bologna@yahoo.it>
2025-03-18 18:29:47 +0100

    destructuring-predefinito

---
+++ esercizio destructuring con valori predefiniti/index.js
@@ -0,0 +1,13 @@
+const numeri = [10, 20, 30]; 
+
+const [primo = 0, secondo = 0, terzo = 0, quarto = 0, quinto = 0] = numeri;
+
+console.log("Primo elemento:", primo);
+
+console.log("Secondo elemento:", secondo);
+
+console.log("Terzo elemento:", terzo);
+
+console.log("Quarto elemento:", quarto);
+
+console.log("Quinto elemento:", quinto);
