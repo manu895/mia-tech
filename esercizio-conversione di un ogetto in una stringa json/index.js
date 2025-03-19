3fb6a95ad4f0887696a7d79167b3dd9ceef49000
manu895 <manuel_bologna@yahoo.it>
2025-03-19 12:29:07 +0100

    conversione

---
+++ esercizio-conversione di un ogetto in una stringa json/index.js
@@ -0,0 +1,12 @@
+let persona = {
+    nome: "Manuel",
+    età: 33,
+    professione: "Sviluppatore",
+    città: "Piazza Armerina",
+    paese: "Italia"
+  };
+  
+  let personaJSON = JSON.stringify(persona);
+  
+  console.log(personaJSON);
+  
