9cb86bf1b9a6c386e520536e7647db01e59d9b7e
manu895 <manuel_bologna@yahoo.it>
2025-03-19 12:29:22 +0100

    conversione

---
+++ esercizio conversione di una stringa json in oggetto/index.js
@@ -0,0 +1,6 @@
+let personaJSON = '{"nome":"Manuel","età":33,"professione":"Sviluppatore","città":"Piazza Armerina","paese":"Italia"}';
+
+let persona = JSON.parse(personaJSON);
+
+
+console.log(persona);
