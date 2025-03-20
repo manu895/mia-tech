987db5794cfcf055070fa038d460dc4efbfcd6a0
manu895 <manuel_bologna@yahoo.it>
2025-03-20 17:12:14 +0100

    console.log

---
+++ esecizio utilizzare un console.log/index.js
@@ -0,0 +1,12 @@
+let numero = 42;
+let stringa = "Ciao, mondo!";
+let array = [1, 2, 3, 4, 5];
+let oggetto = { nome: "Manuel", età: 33, città: "Piazza Armerina" }; 
+
+console.log(numero);
+
+console.log(stringa);
+
+console.log(array);
+
+console.log(oggetto);
