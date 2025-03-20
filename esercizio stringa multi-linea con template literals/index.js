e49926012c35cf76cba5647784185781423e5fa9
manu895 <manuel_bologna@yahoo.it>
2025-03-20 17:11:43 +0100

    template

---
+++ esercizio stringa multi-linea con template literals/index.js
@@ -0,0 +1,11 @@
+let nome = "Manuel";
+let cognome = "Bologna";
+let età = 33;
+let città = "Piazza Armerina";
+
+let info = `Nome: ${nome}
+Cognome: ${cognome}
+Età: ${età}
+Città: ${città}`;
+
+console.log(info);
