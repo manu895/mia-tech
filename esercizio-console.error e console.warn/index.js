4b3c763d22fceb02c9254a72c11ca89810958c7c
manu895 <manuel_bologna@yahoo.it>
2025-03-20 17:12:31 +0100

    console.warn

---
+++ esercizio-console.error e console.warn/index.js
@@ -0,0 +1,12 @@
+let stato = "errore";
+
+
+if (stato === "errore") {
+  console.error("Si è verificato un errore!");
+
+} else if (stato === "avviso") {
+
+  console.warn("Questo è solo un avviso!");
+  
+  console.log("Tutto è a posto!");
+}
