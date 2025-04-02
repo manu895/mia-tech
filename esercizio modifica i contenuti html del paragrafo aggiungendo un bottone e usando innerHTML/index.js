d730d87ba8ff38bdff87117c446b466e7f79e6fc
manu895 <manuel_bologna@yahoo.it>
2025-04-02 11:33:26 +0200

    innerhtml

---
+++ esercizio modifica i contenuti html del paragrafo aggiungendo un bottone e usando innerHTML/index.js
@@ -0,0 +1,10 @@
+const paragrafo = document.getElementById("paragrafo");
+
+// cambio del colore e della dimensione del font
+paragrafo.style.backgroundColor = "yellow";
+
+paragrafo.style.fontSize = "20px";
+
+// modificat html aggiungendo un bottone con innerhtml
+
+paragrafo.innerHTML = 'Questo è un paragrafo con un bottone <button>Clicca qui</button>';
