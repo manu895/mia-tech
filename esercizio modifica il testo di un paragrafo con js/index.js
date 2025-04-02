487e6de3d10eac590c09135dffa12eda07fb663a
manu895 <manuel_bologna@yahoo.it>
2025-04-02 11:27:53 +0200

    paragrafo js

---
+++ esercizio modifica il testo di un paragrafo con js/index.js
@@ -0,0 +1,6 @@
+const paragrafo = document.getElementById("paragrafo");
+
+// cambio del colore e della dimensione del font
+paragrafo.style.backgroundColor = "yellow";
+
+paragrafo.style.fontSize = "20px";
