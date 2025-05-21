5048b07bb8bfc97c048f1dcfbd56f030dc9aa34e
manu895 <manuel_bologna@yahoo.it>
2025-05-21 09:54:09 +0200

    deleted file

--- esercizi react dall'1 al 4 hello world/src/Title.jsx
+++
@@ -1,5 +0,0 @@
-function Title() {
-    return <h1>Hello world</h1>;
-}
-
-export default Title;
--- esercizi react dall'1 al 4 hello world/src/text.jsx
+++
@@ -1,5 +0,0 @@
-function Text() {
-    return <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor deserunt ratione consectetur animi tempora asperiores quam sed? Expedita, quasi voluptate ipsa beatae, minima, aut fugiat quia cum officiis saepe quam.</p>
-}
-
-export default Text;
